d178137f31a9cd8f4041779eedf96a6d368c519f
Sergey Tovt <st@norse.co>
2019-10-06 12:10:29 +0300

    store completed exercises on firestore

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -45,7 +45,7 @@ export class TrainingService {
   }
 
   completeExercise() {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       date: new Date(),
       state: 'completed'
@@ -55,7 +55,7 @@ export class TrainingService {
   }
 
   cancelExercise(progress: number) {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       duration: this.runningExercise.duration * (progress / 100),
       calories: this.runningExercise.calories * (progress / 100),
@@ -72,5 +72,9 @@ export class TrainingService {
 
   getCompletedOrCancelledExercises() {
     return this.exercises.slice();
+  }
+
+  private addDataToDatabase(exercise: Exercise) {
+    this.db.collection('finisedExercises').add(exercise);
   }
 }
