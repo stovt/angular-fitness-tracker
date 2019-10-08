3897037a4235aca5bcab094b3ca397281680a3c4
Sergey Tovt <st@norse.co>
2019-10-08 10:44:05 +0300

    update lint rules

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { MatSnackBar } from '@angular/material';
 import { Subject } from 'rxjs';
 
 import { AuthData } from './auth-data.model';
@@ -18,7 +17,6 @@ export class AuthService {
     private router: Router,
     private afAuth: AngularFireAuth,
     private trainingService: TrainingService,
-    private snackbar: MatSnackBar,
     private uiService: UIService
   ) {}
 
